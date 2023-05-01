5c0ea71172a2efad0748e7980e15dae94f5169b4
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-01 16:23:50 +0600

    skills section add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Skills from './components/Skills/Skills'
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
       <main className="main">
         <Home></Home>
         <About></About>
+        <Skills></Skills>
       </main>
     </>
   )
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div>
+            <h2>this is skills page</h2>
+        </div>
+    );
+};
+
+export default Skills;
