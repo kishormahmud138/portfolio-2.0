78974375ae24b902bc22ff8fa2676d34bc9e5570
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-05 23:22:07 +0600

    complete-services section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
 
 const Services = () => {
+  const [modal, setModal] = useState(0);
+
+  const toggleModal = (index) => {
+    setModal(index);
+  };
   return (
     <section className="services section" id="services">
       <h2 className="section-title">Services</h2>
       <span className="section-subtitle">What I Offer</span>
 
       <div className="services-container container grid">
-        <div className="services content">
+        <div className="services-content">
           <div>
             <i className="uil uil-web-grid services-icon"></i>
             <h3 className="services title">
@@ -16,14 +21,21 @@ const Services = () => {
             </h3>
           </div>
 
-          <span className="services-button">
+          <span className="services-button" onClick={() => toggleModal(1)}>
             View More
             <i className="uil uil-arrow-right services-button-icon"></i>
           </span>
 
-          <div className="services-modal">
+          <div
+            className={
+              modal === 1 ? "services-modal active-modal" : "services-modal"
+            }
+          >
             <div className="services-modal-content">
-              <i className="uil uil-times services-modal-close"></i>
+              <i
+                onClick={() => toggleModal(0)}
+                className="uil uil-times services-modal-close"
+              ></i>
 
               <h3 className="services-modal-title">Product Designer</h3>
               <p className="services-modal-description">
@@ -32,35 +44,35 @@ const Services = () => {
               </p>
 
               <ul className="services-modal-services grid">
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
@@ -71,7 +83,7 @@ const Services = () => {
           </div>
         </div>
 
-        <div className="services content">
+        <div className="services-content">
           <div>
             <i className="uil uil-arrow services-icon"></i>
             <h3 className="services title">
@@ -79,14 +91,21 @@ const Services = () => {
             </h3>
           </div>
 
-          <span className="services-button">
+          <span className="services-button" onClick={() => toggleModal(2)}>
             View More
             <i className="uil uil-arrow-right services-button-icon"></i>
           </span>
 
-          <div className="services-modal">
+          <div
+            className={
+              modal === 2 ? "services-modal active-modal" : "services-modal"
+            }
+          >
             <div className="services-modal-content">
-              <i className="uil uil-times services-modal-close"></i>
+              <i
+                onClick={() => toggleModal(0)}
+                className="uil uil-times services-modal-close"
+              ></i>
 
               <h3 className="services-modal-title">UI/UX Designer</h3>
               <p className="services-modal-description">
@@ -95,35 +114,35 @@ const Services = () => {
               </p>
 
               <ul className="services-modal-services grid">
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
@@ -134,7 +153,7 @@ const Services = () => {
           </div>
         </div>
 
-        <div className="services content">
+        <div className="services-content">
           <div>
             <i className="uil uil-edit services-icon"></i>
             <h3 className="services title">
@@ -142,14 +161,21 @@ const Services = () => {
             </h3>
           </div>
 
-          <span className="services-button">
+          <span className="services-button" onClick={() => toggleModal(3)}>
             View More
             <i className="uil uil-arrow-right services-button-icon"></i>
           </span>
 
-          <div className="services-modal">
+          <div
+            className={
+              modal === 3 ? "services-modal active-modal" : "services-modal"
+            }
+          >
             <div className="services-modal-content">
-              <i className="uil uil-times services-modal-close"></i>
+              <i
+                onClick={() => toggleModal(0)}
+                className="uil uil-times services-modal-close"
+              ></i>
 
               <h3 className="services-modal-title">Visual Designer</h3>
               <p className="services-modal-description">
@@ -158,35 +184,35 @@ const Services = () => {
               </p>
 
               <ul className="services-modal-services grid">
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
                   </p>
                 </li>
 
-                <li className="services-modal-services">
+                <li className="services-modal-services-item">
                   <i className="uil uil-check-circle services-modal-icon"></i>
                   <p className="services-modal-info">
                     I develop the user Interface
