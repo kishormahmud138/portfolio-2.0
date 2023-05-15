a038862adb6a264a4f77d9ae9c89892c6b63456e
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-15 23:29:02 +0600

    add qualification section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
-import About from './components/About/About'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Services from './components/Services/Services'
-import Skills from './components/Skills/Skills'
+import "./App.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Qualification from "./components/Qualification/Qualification";
+import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
 
 function App() {
-  
-
   return (
     <>
       <Header></Header>
@@ -18,9 +16,10 @@ function App() {
         <About></About>
         <Skills></Skills>
         <Services></Services>
+        <Qualification></Qualification>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Qualification/Qualification.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import "./Qualification.css";
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+      <h2 className="section-title">Qualification</h2>
+      <span className="section-subtitle">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, illo?
+      </span>
+
+      <div className="qualification-container container">
+        <div className="qualification-tabs">
+          <div className="qualification-button button-flex">
+            <i className="uil uil-graduation-cap qualification-icon"></i>{" "}
+            Education
+          </div>
+
+          <div className="qualification-button button-flex">
+            <i className="uil uil-briefcase-alt qualification-icon"></i>{" "}
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification-section">
+          <div className="qualification-content qualification-content-active">
+            <div className="qualification-data">
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="qualification-content">
+            <div className="qualification-data">
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+            </div>
+
+            <div className="qualification-data">
+              <div>
+                <span className="qualification-rounder"></span>
+                <span className="qualification-line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification-title">SSC</h3>
+                <span className="qualification-subtitle">Deuty school</span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
