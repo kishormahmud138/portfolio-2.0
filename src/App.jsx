1a8bbf9d87fe529dfa8770711d78531259e189e6
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-18 18:51:22 +0600

    complete testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Qualification from "./components/Qualification/Qualification";
@@ -19,6 +20,7 @@ function App() {
         <Services></Services>
         <Qualification></Qualification>
         <Testimonial></Testimonial>
+        <Contact></Contact>
       </main>
     </>
   );
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section-title">Contact Me</h2>
+      <span className="section-subtitle">Say Hello</span>
+    </section>
+  );
+};
+
+export default Contact;
