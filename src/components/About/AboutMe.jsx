adbac1c7546ef126b8345b2cde92d097132df97d
Kishor Mahmud <dev.kishor138@gmail.com>
2023-06-01 18:21:35 +0600

    add some text in about me section.

---
+++ src/components/About/AboutMe.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="grid about-me">
+      <p>
+        Hello, I am Kishor Mahmud. I am a Full Stack Web Developer. I have a
+        strong passion about Web Development. When I completed the Web
+        Development Course With React. Here I gained a solid foundation in web
+        development principles, languages, and technologies, including HTML,
+        CSS, JavaScript, and frameworks like React. <br /> <br /> Then I did a
+        6-month internship as a junior developer in E-Learning and Earning LTD.
+        Here I get improve my skill in ​​Full MERN Stack Development. Then I
+        create some projects. Now I am improving my skill with new technology.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -1,13 +1,25 @@
 const Social = () => {
   return (
     <div className="home-social">
-      <a href="" className="home-social-icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/kishor-mahmud-850567274"
+        className="home-social-icon"
+        target="_blank"
+      >
         <i class="uil uil-linkedin-alt"></i>
       </a>
-      <a href="" className="home-social-icon" target="_blank">
+      <a
+        href="https://www.facebook.com/kishor.mahmud.009"
+        className="home-social-icon"
+        target="_blank"
+      >
         <i class="uil uil-facebook-f"></i>
       </a>
-      <a href="" className="home-social-icon" target="_blank">
+      <a
+        href="https://github.com/kishormahmud138"
+        className="home-social-icon"
+        target="_blank"
+      >
         <i className="uil uil-github-alt"></i>
       </a>
     </div>
