d11cc7b1f84721720ea0dd358f46e50dbb65bdb1
Kishor Mahmud <dev.kishor138@gmail.com>
2023-06-02 18:37:33 +0600

    add projects picture

--- src/components/Projects/ProjectsData.jsx
+++ src/components/Projects/ProjectsData.jsx
@@ -1,67 +1,81 @@
-import Image1 from "../../assets/work/g2.png";
-import Image2 from "../../assets/work/g4.png";
-import Image3 from "../../assets/work/g6.png";
-import Image4 from "../../assets/work/g9.png";
-import Image5 from "../../assets/work/g1.jpg";
-import Image6 from "../../assets/work/g3.png";
-import Image7 from "../../assets/work/g5.png";
-import Image8 from "../../assets/work/g7.jpg";
-import Image9 from "../../assets/work/g8.png";
+import Image1 from "../../assets/Projects/project13.png";
+import Image2 from "../../assets/Projects/project2.png";
+import Image3 from "../../assets/Projects/project3.png";
+import Image4 from "../../assets/Projects/project4.png";
+import Image5 from "../../assets/Projects/project5.png";
+import Image6 from "../../assets/Projects/project7.png";
+import Image7 from "../../assets/Projects/project8.png";
+import Image8 from "../../assets/Projects/project9.png";
+import Image9 from "../../assets/Projects/project10.png";
+import Image10 from "../../assets/Projects/project11.png";
+import Image11 from "../../assets/Projects/project12.png";
 
 export const projectsData = [
   {
     id: 1,
     image: Image1,
-    title: "Web Design",
-    category: "web",
+    title: "Dev Portfolio",
+    category: "react",
   },
   {
     id: 2,
     image: Image2,
-    title: "Web Design",
-    category: "app",
+    title: "Food Donate",
+    category: "website",
   },
   {
     id: 3,
     image: Image3,
-    title: "Web Design",
-    category: "design",
+    title: "Football Maniya",
+    category: "website",
   },
   {
     id: 4,
     image: Image4,
-    title: "Web Design",
-    category: "web",
+    title: "Country Guide",
+    category: "design",
   },
   {
     id: 5,
     image: Image5,
-    title: "Web Design",
+    title: "Analog Watch",
     category: "design",
   },
   {
     id: 6,
     image: Image6,
-    title: "Web Design",
-    category: "app",
+    title: "Digital Clock",
+    category: "design",
   },
   {
     id: 7,
     image: Image7,
-    title: "Web Design",
-    category: "web",
+    title: "Password Strength Checker",
+    category: "design",
   },
   {
     id: 8,
     image: Image8,
-    title: "Web Design",
-    category: "design",
+    title: "Ema-John",
+    category: "react",
   },
   {
     id: 9,
     image: Image9,
-    title: "Web Design",
-    category: "web",
+    title: "College Portfolio",
+    category: "website",
+  },
+  {
+    id: 10,
+    image: Image10,
+    title: "Programming Hero Clone",
+    category: "website",
+  },
+  {
+    id: 11,
+    image: Image11,
+    title: "Looplab",
+    category: "website",
   },
 ];
 
